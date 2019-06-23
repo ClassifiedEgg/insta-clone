8f2b0668d576a3306177d4dd37e85263cd0a1784
ClassifiedEgg <ashishajr@gmail.com>
2019-06-23 20:36:15 +0300

    added REST to post - no css done

--- app.js
+++ app.js
@@ -1,133 +1,212 @@
 var express = require("express"),
-    app = express(),
-    mongoose = require("mongoose"),
-    bodyParser = require("body-parser"),
-    User = require("./models/user"),
-    Post = require("./models/post"),
-    session = require("express-session"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local");
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  User = require("./models/user"),
+  Post = require("./models/post"),
+  session = require("express-session"),
+  passport = require("passport"),
+  methodOverride = require("method-override"),
+  LocalStrategy = require("passport-local"),
+  isImageUrl = require("is-image-url");
 
-mongoose.connect('mongodb://localhost:27017/instaclone', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/instaclone", {
+  useNewUrlParser: true
+});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
-app.use(session({
+app.use(
+  session({
     secret: "first rule of seceret is dont tell anyone",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    next();
+  res.locals.currentUser = req.user;
+  next();
 });
 
-
-app.get("/", function (req, res) {
-    res.render("landing");
+//========================= LANDING PAGE =========================
+app.get("/", function(req, res) {
+  res.render("landing");
 });
 
-
-app.get("/register", function (req, res) {
-    res.render("signup");
-    console.log(req.body.user);
+//========================= SIGNUP PAGE =========================
+app.get("/register", function(req, res) {
+  res.render("user/signup");
+  console.log(req.body.user);
 });
 
-app.post("/register", function (req, res) {
-    var newUser = new User({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        avatar: req.body.avatar
-    });
-    User.register(newUser, req.body.password, function (err, user) {
-        if (err) {
-            console.log(err);
-        } else {
-            passport.authenticate("local")(req, res, function () {
-                res.redirect("/");
-            });
-        }
-    });
+app.post("/register", function(req, res) {
+  var newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar
+  });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/");
+      });
+    }
+  });
 });
 
-app.get("/login", function (req, res) {
-    res.render("login");
-})
-
-app.post("/login", function (req, res) {
-    passport.authenticate("local", function (err, user, info) {
-        if (err) {
-            console.log(err);
-        }
-        if (!user) {
-            return res.redirect('/login');
-        } else {
-            req.logIn(user, function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect("/");
-                }
-            });
-        }
-    })(req, res);
+//========================= LOGIN PAGE =========================
+app.get("/login", function(req, res) {
+  res.render("user/login");
 });
 
-app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
+
+//========================= LOGOUT ROUTE =========================
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
-
-app.get("/:id", function (req, res) {
-    User.findById(req.params.id).populate("posts").exec(function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("main", { user: foundUser });
-        }
+//========================= MAIN USER PAGE =========================
+app.get("/:id", function(req, res) {
+  User.findById(req.params.id)
+    .populate("posts")
+    .exec(function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("user/main", { user: foundUser });
+      }
     });
 });
 
-app.post("/:id", function (req, res) {
-    User.findById(req.params.id, function (err, foundUser) {
+//========================= NEW POST PAGE =========================
+app.post("/:id", function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      var newPost = new Post({
+        image: req.body.imageurl,
+        caption: req.body.caption
+      });
+      Post.create(newPost, function(err, madePost) {
+        madePost.author.id = req.user._id;
+        madePost.author.username = req.user.username;
+        madePost.save();
+        foundUser.posts.push(madePost);
+        foundUser.save();
+        res.redirect("/" + foundUser._id);
+      });
+    }
+  });
+});
+
+app.get("/:id/new", function(req, res) {
+  res.render("post/new");
+});
+
+//========================= EDIT USER PAGE =========================
+app.get("/:id/edit", function(req, res) {
+  res.render("user/edit");
+});
+
+app.put("/:id", function(req, res) {
+  var updatedUser = {
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar
+  };
+  User.findByIdAndUpdate(req.params.id, { $set: updatedUser }, function(
+    err,
+    postUpdateUser
+  ) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/" + postUpdateUser._id);
+    }
+  });
+});
+
+//========================= SHOW POST PAGE =========================
+app.get("/:id/post/:postid", function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      Post.findById(req.params.postid, function(err, foundPost) {
         if (err) {
-            console.log(err);
+          console.log(err);
+          res.redirect("back");
         } else {
-            var newPost = new Post({
-                image: req.body.imageurl,
-                caption: req.body.caption
-            });
-            Post.create(newPost, function (err, madePost) {
-                madePost.author.id = req.user._id;
-                madePost.author.username = req.user.username;
-                madePost.save();
-                foundUser.posts.push(madePost);
-                foundUser.save();
-                res.redirect("/");
-            });
+          res.render("post/show", { user: foundUser, post: foundPost });
         }
-    });
+      });
+    }
+  });
 });
 
-app.get("/:id/new", function (req, res) {
-    res.render("new");
+//========================= EDIT POST PAGE =========================
+app.get("/:id/post/:postid/edit", function(req, res) {
+  Post.findById(req.params.postid, function(err, foundPost){
+    res.render("post/edit", {post: foundPost});
+  });
+});
+
+app.put("/:id/post/:postid", function(req, res) {
+  var updatedPost = {
+    caption: req.body.caption
+  };
+  Post.findByIdAndUpdate(req.params.postid, { $set: updatedPost }, function(
+    err,
+    postUpdatedPost
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/" + req.params.id + "/post/" + req.params.postid);
+    }
+  });
+});
+
+//========================= DELETE POST PAGE =========================
+app.delete("/:id/post/:postid", function(req, res){
+  Post.findByIdAndRemove(req.params.postid, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/" + req.params.id);
+    }
+  });
 });
 
 
-
-app.listen(3000, process.env.IP, function () {
-    console.log("The server has satrted");
-})
+app.listen(3000, process.env.IP, function() {
+  console.log("The server has started");
+});
